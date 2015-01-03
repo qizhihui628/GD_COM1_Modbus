01b4b0d2c88e7f34856178ed995d49365ea061e8
qizhihui628 <q8471@163.com>
2015-01-03 15:28:04 +0800

    1. ARM: add libsqlite* sqlite3.h 2. Debug -> x86 , Release -> ARM

---
+++ COMx/Release/subdir.mk
@@ -0,0 +1,54 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../crc_check.c \
+../dev_XR75CX_module.c \
+../dev_daikin_module.c \
+../dev_ligth_mod.c \
+../dev_power_module.c \
+../main.c \
+../modbus_master.c \
+../nonstd_master.c \
+../serial.c \
+../sql_op.c \
+../system.c 
+
+OBJS += \
+./crc_check.o \
+./dev_XR75CX_module.o \
+./dev_daikin_module.o \
+./dev_ligth_mod.o \
+./dev_power_module.o \
+./main.o \
+./modbus_master.o \
+./nonstd_master.o \
+./serial.o \
+./sql_op.o \
+./system.o 
+
+C_DEPS += \
+./crc_check.d \
+./dev_XR75CX_module.d \
+./dev_daikin_module.d \
+./dev_ligth_mod.d \
+./dev_power_module.d \
+./main.d \
+./modbus_master.d \
+./nonstd_master.d \
+./serial.d \
+./sql_op.d \
+./system.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross GCC Compiler'
+	arm-linux-gcc -I/home/jeqi/git/GD_COM1_Modbus/COMx/lib -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
